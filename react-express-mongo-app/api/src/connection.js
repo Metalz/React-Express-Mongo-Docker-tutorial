52ae9d9fe07fdc65255eb752353e169bd5e08cc1
Zino <metalzino@gmail.com>
2020-03-02 16:31:56 +0100

    update ip

--- react-express-mongo-app/api/src/connection.js
+++ react-express-mongo-app/api/src/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const User = require("./User.model");
 
-const connection = "mongodb://mongo:27017/mongo-test";
+/* const connection = "mongodb://mongo:27017/mongo-test"; */
+const connection = "mongodb://217.160.212.154:27017/my_db";
 
 const connectDb = () => {
   return mongoose.connect(connection);
