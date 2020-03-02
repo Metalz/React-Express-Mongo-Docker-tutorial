f848ab150e5979df133dd60521321910e4bdeaf5
Zino <metalzino@gmail.com>
2020-03-02 19:44:45 +0100

    update ip 3

--- react-express-mongo-app/api/src/connection.js
+++ react-express-mongo-app/api/src/connection.js
@@ -5,6 +5,11 @@ const User = require("./User.model");
 /* const connection = "mongodb://mongo:27017/mongo-test"; */
 const connection = "mongodb://217.160.212.154:27017/my_db";
 
+mongoose.set("useNewUrlParser", true);
+/* mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true); */
+
 const connectDb = () => {
   return mongoose.connect(connection);
 };
